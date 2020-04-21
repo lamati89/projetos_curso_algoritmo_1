83080266430bb01397e2571d097140ed656a642a
lamati89 <lais_amati@hotmail.com>
2020-04-20 21:31:15 -0300

    Primeiro commite do código

---
+++ operadores-relacionais.js
@@ -0,0 +1,22 @@
+/*Var
+// Seção de Declarações das variáveis 
+   nota01, nota02: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   nota01 :=10
+   nota02 :=15
+   
+   se(nota01 <> nota02) então
+     escreval ("Nota01 é diferente)
+   senao
+     escreval ("Nota01 é igual")
+   fimse
+   */
+
+   var nota01, nota02: real
+
+if (nota01 ==  nota02)
+     alert ("Nota01 é diferente")
+else
+     alert ("Nota01 é igual")
